c1d8d7d659faff7d8293f70e30e9ccedff75e72b
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-03-31 13:42:17 +0900

    [level 3] Title: 스티커 모으기(2), Time: 66.39 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -15,6 +15,8 @@ def solution(sticker):
     if (len(sticker) == 1):
         return sticker[0]
     
+    # 만약 한번만 dp 수행하면 sticker[0]이 선택되었을 시 sticker[-1]은 항상 선택이 불가하므로 sticker[0]을 선택하는 경우와 sticker[-1]를 선택하는 경우를 나누어 dp 수행
+    # 이를 위해 index 0이 포함된 subArray와 -1이 포함된 subArray로 나눔
     firstArray = sticker[0:len(sticker)-1]
     secondArray = sticker[1:len(sticker)]
     
