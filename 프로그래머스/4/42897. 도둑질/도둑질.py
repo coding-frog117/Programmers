bc519f868d130d5952c214022e5ab945181fe392
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-04-01 09:06:14 +0900

    [level 4] Title: 도둑질, Time: 594.10 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -0,0 +1,21 @@
+def dp(subArray):
+    if (len(subArray) == 1):
+        return subArray[0]
+    
+    count = [0 for _ in range(len(subArray))]
+    count[0] = subArray[0]
+    count[1] = max(subArray[0],subArray[1])
+    
+    for i in range(2, len(subArray)):
+        count[i] = max(count[i-1], count[i-2]+ subArray[i])
+
+    return count[-1]
+
+def solution(money):
+    # 만약 한번만 dp 수행하면 sticker[0]이 선택되었을 시 sticker[-1]은 항상 선택이 불가하므로 sticker[0]을 선택하는 경우와 sticker[-1]를 선택하는 경우를 나누어 dp 수행
+    # 이를 위해 index 0이 포함된 subArray와 -1이 포함된 subArray로 나눔
+    firstArray = money[0:len(money)-1]
+    secondArray = money[1:len(money)]
+    
+    answer = max(dp(firstArray), dp(secondArray))
+    return answer
