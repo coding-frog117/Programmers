3d85ff86115fb61deb82fe5cc83c4415d622a71b
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-04-03 13:14:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -2,25 +2,25 @@ import heapq
 
 def solution(operations):
     minheap=[]
-    maxheap=[]
     
     for operation in operations:
         op1, op2 = operation.split(' ')
         op2= int(op2)
         if (op1 == 'I'):
             heapq.heappush(minheap,op2)
-            heapq.heappush(maxheap,int(op2)*-1)
             
         elif (op1 == 'D' and op2 == 1 and len(minheap)>0):
-            maxData = heapq.heappop(maxheap)
-            minheap.remove(-maxData)
+#             heapq.nlargest 메소드로 최댓값부터 추출해냄, 최댓값인 인덱스 0을 제외하고 새로운 배열 생성
+            newheap = heapq.nlargest(len(minheap),minheap)[1:]
+#     최솟값 우선순위큐로 만들어줌
+            heapq.heapify(newheap)
+            minheap = newheap
         
         elif (op1 == 'D' and op2 == -1 and len(minheap)>0):
-            minData = heapq.heappop(minheap)
-            maxheap.remove(-minData)
+            heapq.heappop(minheap)
     
-    maxAnswer = -maxheap[0] if len(maxheap) > 0 else 0
-    minAnswer = minheap[0] if len(maxheap) > 0 else 0
+    maxAnswer = heapq.nlargest(1,minheap)[0] if len(minheap) > 0 else 0
+    minAnswer = minheap[0] if len(minheap) > 0 else 0
     answer = [maxAnswer,minAnswer]
     
     return answer
