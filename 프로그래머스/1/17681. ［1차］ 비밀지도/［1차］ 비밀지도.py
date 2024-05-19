640524d61289890ea6b59d480a9afdd33dc81954
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-05-19 22:12:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,30 @@
+def solution(n, arr1, arr2):
+    map1 = []
+    map2 = []
+    for num in arr1:
+        invert = str(bin(num))[2:]
+        if len(invert) < n:
+            plusStr = n-len(invert)
+            plusStr = '0' * plusStr
+            invert = plusStr + invert
+        map1.append(invert)
+    
+    for num in arr2:
+        invert = str(bin(num))[2:]
+        if len(invert) < n:
+            plusStr = n-len(invert)
+            plusStr = '0' * plusStr
+            invert = plusStr + invert
+        map2.append(invert)
+    
+    ansMap = ['' for i in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            ans = '1' if (map1[i][j] == '1' or map2[i][j] =='1') else '0'
+            if ans == '1':
+                ansMap[i] +='#'
+            else:
+                ansMap[i] +=' '
+                
+    return ansMap
