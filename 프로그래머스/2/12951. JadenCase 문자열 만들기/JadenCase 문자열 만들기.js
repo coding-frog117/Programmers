ba0604bdaf974632f5a6ab519bcd4bce45e4b701
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-06-21 23:05:56 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,29 @@
+function solution(s) {
+    answer = []
+    s = s.toLowerCase()
+    stringArr = s.split(' ')
+    console.log(stringArr)
+    stringArr.forEach((str,index)=>{
+        console.log(str)
+        if (str == '' || str == ' '){
+            answer.push('')
+        }
+        else if (isNaN(str[0])) {
+            upper = str[0].toUpperCase()
+            if (str.length > 1){
+                answer.push(upper + str.slice(1,))
+            }
+            else{
+                answer.push(upper)
+            }
+        }
+        else{
+            if (str.length > 1){
+                answer.push(str[0] + str.slice(1,))
+            }else{
+                answer.push(str[0])
+            }
+        }
+    })
+    return answer.join(' ')
+}
