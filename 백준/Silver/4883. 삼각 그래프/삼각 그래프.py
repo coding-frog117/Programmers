cff690328e3b15023ab36cb9d3927f862de7c31a
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-07-11 20:41:20 +0900

    [Silver I] Title: 삼각 그래프, Time: 3064 ms, Memory: 52380 KB -BaekjoonHub

---
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.py
@@ -0,0 +1,37 @@
+import sys
+
+tc = 1
+while True:
+  N= int(input())
+  if N == 0:
+    break
+
+  graph = []
+  dp = [[sys.maxsize]*3 for i in range(2)]
+  for i in range(N):
+    col = list(map(int,input().split()))
+    graph.append(col)
+
+  dp[0][0] = graph[0][0]
+  dp[0][1] = graph[0][1]
+  dp[0][2] = graph[0][1] + graph[0][2]
+
+  dp[1][0] = graph[0][1] + graph[1][0]
+  dp[1][1] = min(min(dp[0][1],dp[0][2]),dp[1][0]) + graph[1][1]
+  dp[1][2] = min(min(dp[0][1],dp[0][2]),dp[1][1]) + graph[1][2]
+
+  dp[0][0] = dp[1][0]
+  dp[0][1] = dp[1][1]
+  dp[0][2] = dp[1][2]
+
+  for i in range(2,N):
+    dp[1][0] = min(dp[0][0],dp[0][1]) + graph[i][0]
+    dp[1][1] = min(min(dp[0][0],dp[0][1],dp[0][2]),dp[1][0]) + graph[i][1]
+    dp[1][2] = min(min(dp[0][1],dp[0][2]),dp[1][1]) + graph[i][2]
+
+    dp[0][0] = dp[1][0]
+    dp[0][1] = dp[1][1]
+    dp[0][2] = dp[1][2]
+
+  print(f'{tc}. {dp[1][1]}')
+  tc+= 1
