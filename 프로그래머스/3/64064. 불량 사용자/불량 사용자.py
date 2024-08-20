89ac9b2cb3198aa4a18d7f2883043fb8741ba8ba
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-08-20 21:18:16 +0900

    [level 3] Title: 불량 사용자, Time: 8538.05 ms, Memory: 1.91 GB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,26 +1,23 @@
-from itertools import permutations
+from itertools import product
 
-def isPossible(curr,ban):
-    if len(curr) != len(ban):
-        return False
-    for i in range(len(ban)):
-        if ban[i] !='*' and curr[i] != ban[i]:
-            return False
-    else:
-        return True
-    
 def solution(user_id, banned_id):
-    ans_set = set()
-    permu_id = list(permutations(user_id,len(banned_id)))
-    banned_id.sort(key = lambda x:len(x))
+    ans_set = []
+    for ban in banned_id:
+        participants = []
+        for user in user_id:
+            if len(user) != len(ban):
+                continue
+            for i in range(len(ban)):
+                if ban[i] !='*' and user[i] != ban[i]:
+                    break
+            else:
+                participants.append(user)
+        ans_set.append(participants)
+    products = list(product(*ans_set))
+    ans = set()
     
-    for combi in permu_id:
-        combi = list(combi)
-        combi.sort(key = lambda x:len(x))
-        for i in range(len(banned_id)):
-            if not isPossible(combi[i],banned_id[i]):
-                break
-        else:
-            combi.sort()
-            ans_set.add(tuple(combi))
-    return len(ans_set)
+    for i in products:
+        if len(set(i)) == len(banned_id) :
+            ans.add("".join(sorted(set(i))))
+    return len(ans)
+        
