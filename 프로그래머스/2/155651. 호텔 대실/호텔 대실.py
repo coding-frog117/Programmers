79c0419472117141da90bf15078be592215c708f
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-08-23 14:57:14 +0900

    [level 2] Title: 호텔 대실, Time: 37.50 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,62 +1,14 @@
 def solution(book_time):
-    room = 1
-    empty = [[['00:00','23:59']]]
-    book_time.sort()
-    
+    reservations = [0]*1440
     for time in book_time:
         [start,end] = time
         s_h,s_m = start.split(':')
         e_h,e_m = end.split(':')
-        isPossible = False
-        
-        for roomidx,cur_room in enumerate(empty):
-            roomFill = False
-            for idx,emp_time in enumerate(cur_room):
-                [emp_s,emp_e] = emp_time
-                emp_s_h, emp_s_m = emp_s.split(':')
-                emp_e_h, emp_e_m = emp_e.split(':')
-                
-                if not(int(emp_s_h) < int(s_h) or (int(emp_s_h) == int(s_h) and int(emp_s_m) <= int(s_m))):
-                    continue
-                if not(int(emp_e_h) > int(e_h) or (int(emp_e_h)==  int(e_h) and int(emp_e_m) >= int(e_m))):
-                    continue
-                isPossible = True
-                roomFill = True
-                addTime = []
-                if emp_s != start:
-                    time_h = int(s_h)
-                    time_m = int(s_m)-10
-                    if time_m < 0:
-                        time_m = 60 + time_m
-                        time_h -= 1
-                    addTime.append([emp_s,f'{time_h}:{time_m}'])
-                if emp_e != end:
-                    time_h = int(e_h)
-                    time_m = int(e_m)+10
-                    if time_m >= 60:
-                        time_m = time_m - 60
-                        time_h += 1
-                    addTime.append([f'{time_h}:{time_m}',emp_e])
-                    
-                empty[roomidx] = empty[roomidx][0:idx] + addTime + empty[roomidx][idx+1:]
-            if roomFill:
-                break
-                
-        if not isPossible :
-            empty.append([])
-            if emp_s != start:
-                time_h = int(s_h)
-                time_m = int(s_m)-10
-                if time_m < 0:
-                    time_m = 60 +time_m
-                    time_h -= 1
-                empty[-1].append(['00:00',f'{time_h}:{time_m}'])
+        time_start = int(s_h)*60+int(s_m)
+        time_end = int(e_h)*60+int(e_m)+10
+        if time_end > 1439:
+            time_end = 1439
+        for i in range(time_start,time_end):
+            reservations[i] += 1
             
-            if emp_e != end:
-                time_h = int(e_h)
-                time_m = int(e_m)+10
-                if time_m >= 60:
-                    time_m = time_m - 60
-                    time_h += 1
-                empty[-1].append([f'{time_h}:{time_m}','23:59'])
-    return len(empty)
+    return max(reservations)
