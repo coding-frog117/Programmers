fdd20219547f0a93812de0fc34bcc5bf7feb84d3
송예은 <110962765+coding-frog117@users.noreply.github.com>
2024-09-04 23:17:51 +0900

    [level 2] Title: 땅따먹기, Time: 442.75 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,7 +1,7 @@
 def solution(land):
     #DFS로 모든 경로 탐색해서 최대값 return
     #자기자신의 인덱스만 제외하고 선택하기
-    #시간초과뜸 -> DP 사고방식 : 2차원 배열에 각 위치까지의 최댓값 구하기
+    #시간초과뜸 -> DP - 2차원 배열에 각 위치까지의 최댓값 구하기
     
     dp = [[0,0,0,0] for i in range(len(land))]
     for i in range(4):
